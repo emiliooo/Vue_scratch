94a4ff76af91a48a2f9db2e4a6f9772226621358
Emil Buraczynski <emil.buraczynski@decsoft.com.pl>
2018-08-28 16:04:48 +0200

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,48 @@
+<template> 
+    <div class="test"> 
+        <input type="text" v-model="title"/>
+
+        <h1> {{title}}</h1>
+        {{ user.   first_name}}
+          <p v-test="user.last_name">   </p>
+          <p v-if="showName">  {{user.first_name}}   </p>
+          <ul>
+              <li v-for="item in items">  {{ item.title }} </li>
+          </ul>
+       <button v-on:click="greet()"> Say greeting </button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:'test',
+        data() {
+            return {
+                title:'hello world',
+                user: {
+                    first_name : 'Jone',
+                    last_name :' Mikowski'
+                },
+                showName:false,
+                items:[
+                       {title:'one'},
+                       {title:'tree'},
+                       {title:'fore'},
+                       {title:'six'},
+                ]
+            }
+        },
+        methods :{
+            greet: function () {
+                alert('greeting')
+            }
+        }
+    }
+</script>
+
+
+
+<style scoped>
+    
+</style>
